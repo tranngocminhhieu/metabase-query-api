dd68ac9ed499cb4fcd659b0557880f83b3958558
Tran Ngoc Minh Hieu <tnmhieu@gmail.com>
2023-10-07 00:52:23 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+README = (Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name='metabase-query-api',
+    version='1.0.0',
+    description='Metabase Query API with Retry and Bulk Filter Values',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/tranngocminhhieu/metabase-query-api',
+    author='Tran Ngoc Minh Hieu',
+    author_email='tnmhieu@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'tenacity',
+        'nest-asyncio'
+    ]
+)
